ddee9317a2c407b83720f987d8264d2e48a581e6
Tim Macaulay-Robinson <cancerimex@gmail.com>
2018-02-27 00:13:21 -0600

    using composer and autoloader

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,14 +4,6 @@ $app = [];
 
 // Grab the config array
 $app['config'] = require 'config.php';
-// Get the routes
-require 'core/router.php';
-// Request
-require 'core/request.php';
-// Database connection
-require 'database/connection.php';
-// Helps to build SQL Queries
-require 'database/query-builder.php';
 
 // Initialize the QueryBuilder and pass it a DB connection
 $app['database'] = new QueryBuilder(
--- index.php
+++ index.php
@@ -4,6 +4,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Auto load the classes
+require 'vendor/autoload.php';
+
 // Import the bootstrap
 require 'core/bootstrap.php';
 
